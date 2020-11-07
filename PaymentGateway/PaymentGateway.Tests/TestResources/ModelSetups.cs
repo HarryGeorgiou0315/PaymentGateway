d3c61f42b6dda6b23c87c02e8fc40dfa3057343b
HarryGeorgiou0315 <harrygeorgiou96@gmail.com>
2020-11-07 18:18:34 +0000

    Created a very high level bank simulator with mocked responses.
    Hardcoded guids and conditions to be able to test end-to-end.
    Also, updated the CI script.

--- PaymentGateway/PaymentGateway.Tests/TestResources/ModelSetups.cs
+++ PaymentGateway/PaymentGateway.Tests/TestResources/ModelSetups.cs
@@ -6,6 +6,10 @@ namespace PaymentGateway.Tests.TestResources
 {
     public static class ModelSetups
     {
+        // some guids I know will work for testing for success when running integration tests. 
+        public static Guid BankSimulatorValidPaymentId1 = Guid.Parse("cf1f51d5-5cc1-42ab-94f6-a10d0aa25ef3");
+        public static Guid BankSimulatorValidPaymentId2 = Guid.Parse("92752c4b-3ab4-4069-a2eb-613efda81a66");
+
         public static PostPaymentRequestBoundary PostPaymentRequestBoundarySetup = new PostPaymentRequestBoundary
         {
              Address = "123 Test Avenue",
--- PaymentGateway/PaymentGateway/PaymentControllers/PaymentsController.cs
+++ PaymentGateway/PaymentGateway/PaymentControllers/PaymentsController.cs
@@ -4,8 +4,6 @@ using Microsoft.AspNetCore.Mvc;
 using PaymentGateway.PaymentBoundaries;
 using PaymentGateway.PaymentInterfaces;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace PaymentGateway.PaymentControllers
 {
     [Route("api/payment")]
